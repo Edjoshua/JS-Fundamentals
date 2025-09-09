b84a97d275a7a9a3065ded82e8b425be0f30f984
Edjoshua <joshuaedogbo@gmail.com>
2025-09-10 00:12:04 +0100

    Add first JavaScript task and README

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = "JavaScript is amazing";
+console.log(myVar);
+
+# JS-Fundamentals
+
+This repository contains JavaScript exercises for the ALX/holberton School curriculum.
